444639ed6e3d6c444ad886b722802d9837860c46
albertogaia <alberto.gaia95@gmail.com>
2021-09-20 16:21:15 +0200

    Header

--- src/App.vue
+++ src/App.vue
@@ -22,12 +22,7 @@ export default {
 </script>
 
 <style lang="scss">
+@import '@/style/general.scss';
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,29 +1,67 @@
 <template>
-  <div>
-    <h1>HEADER</h1>
-  </div>
+  <header>
+      <div class="header-top"></div>
+      <div class="header-nav container">
+        <div class="logo">
+          <a href="/"><img src="@/assets/img/dc-logo.png" alt="logo.png"></a>
+        </div>
+        <div class="nav-menu">
+          <ul class="nav-items">
+            <li v-for="(link, index) in links" :key="index" class="nav-item" :class="(link.current) ? 'active' : null">
+              <a :href="link.url">{{link.text.toUpperCase()}}</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+  </header>
 </template>
 
 <script>
+import Links from '@/assets/data/Links.js'
 export default {
   name: 'Header',
+  data(){
+    return{
+      links: Links.headerLinks,
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+@import "../style/general";
+
+.header-top{height: 60px; width: 100%; background-color: $secondary-color;}
+.header-nav{height: 100px; display: flex; justify-content: space-between; align-items: center; border-bottom: 4px solid transparent}
+
+.logo {
+  width: 70px;
+  padding: 20px 0px;
+  img{width: 100%;}
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.nav-menu{height: 100%;}
+.nav-items{list-style: none; display: flex; font-size: 12px; font-weight: 600; line-height: 100px;}
+.nav-item{
+  padding: 0px 15px;
+  color: $secondary-color;
+  a{
+    text-decoration: none;
+    color: inherit;
+
+    &:hover{
+      color: $primary-color;
+    }
+
+  }
+
+  &.active{
+      color: $primary-color;
+      border-bottom: 4px solid $primary-color;
+  }
+
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+
+
 </style>
